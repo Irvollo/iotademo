765b914383e18d0d7ca1aa00170359a3afeeb290
irvollo <irvollo@hotmail.com>
2018-01-14 20:55:12 -0600

    Amount and qr code displaying correctly

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 var express = require("express");
 var IOTA = require("iota.lib.js")
 var qr = require("qr-image");
+var path = require("path")
+
 
 //Init app
 var app = express();
@@ -11,12 +13,33 @@ var app = express();
     'provider':'http://iri2.iota.fm:80'
 })
 
+//Set views
+app.set("views", path.resolve(__dirname,"views"));
+app.set("view engine", "ejs");
+
+
+app.get('/new/:amount/:address', function(req, res){
+    var amount = req.param("amount");
+    var address = req.param("address");
+
+    if (iota.valid.isAddress(address)) { 
+        res.render("button", {
+            amount: amount,
+            address: address
+        });
+    } else {
+        res.send("Invalid address")
+    }
+
+});
+
 app.get('/qr/:address', function(req,res){
 
     var address = req.param("address");
-    var code = qr.image(address, {type: 'png', ec_level:'H', size: 10, margin: 0});
-    res.render("button")
-
+    var code = qr.image(address, {type: 'png', ec_level:'H', size: 5, margin: 0});
+    res.setHeader('Content-type', 'image/png');  //sent qr image to client side
+    code.pipe(res);
+    
 });
 
 //Init server
